107d50da62638fc179fff83bd342c7281ac89ca2
LUCKY <luckysingh71826@gmail.com>
2026-08-23 14:01:43 +0530

    Add cava.conf and use install command for missing files copy-paste in launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -2,8 +2,12 @@
 
 # copy the config file if it doesn't exists in user's config dir
 if [[ ! -f "$HOME/.config/chillpill-shell/config.jsonc" ]] && [[ -f /usr/share/chillpill-shell/config.jsonc.example ]]; then
-   mkdir -p "$HOME/.config/chillpill-shell"
-   cp /usr/share/chillpill-shell/config.jsonc.example "$HOME/.config/chillpill-shell/config.jsonc"
+   install -Dm644 /usr/share/chillpill-shell/config.jsonc.example "$HOME/.config/chillpill-shell/config.jsonc"
+fi
+
+# copy the cava config if it doesn't exists in user's cache dir
+if [[ ! -f "$HOME/.cache/chillpill-shell/cava.conf" ]] && [[ -f /usr/share/chillpill-shell/scripts/cava.conf ]]; then
+   install -Dm644 /usr/share/chillpill-shell/scripts/cava.conf "$HOME/.cache/chillpill-shell/cava.conf"
 fi
 
 export LD_LIBRARY_PATH="$HOME/.config/quickshell/chillpill-shell/IslandBackend:$LD_LIBRARY_PATH"
